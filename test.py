0d8c5d69e39591af41bd3bd8e87d56852582bdc4
Maneesh Ghanta <maneeshg@Maneeshs-MacBook-Pro.local>
2025-06-18 18:08:08 -0500

    New changes made for pylint rating by adding return statement

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ def hello():
     """
     This is new changes for rating
     """
-    print("Hello World")
-    Print("Hello World 2")
+    return "Hello World"
+    return "Hello World 2"
 
 hello()
